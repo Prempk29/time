0f5759a8b05e2fa1ccb75aec17dd3de41ac21145
Premkumarnaidu Tadi <prempk.7172@gmail.com>
2022-10-11 22:50:55 +0530

    p

---
+++ script.js
@@ -0,0 +1,26 @@
+setInterval(()=>{
+var prem=document.querySelector(".timemama");
+let date=new Date();
+let hours=date.getHours();
+let minutes=date.getMinutes();
+let seconds=date.getSeconds();
+var day="AM";
+if(hours>12)
+{
+    day="PM";
+    hours=hours%12;
+}
+if(hours<10)
+{
+    hours="0"+hours;
+}
+if(minutes<10)
+{
+    minutes="0"+minutes;
+}
+if(seconds<10)
+{
+    seconds="0"+seconds;
+}
+prem.textContent=hours+":"+minutes+":"+seconds+" " + day;
+});
